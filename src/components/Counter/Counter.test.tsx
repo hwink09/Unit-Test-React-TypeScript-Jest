922b4d504f2439321c252482fc5e99068e48d167
Cao Trần  Hoàng Minh <caotranhoangminh2003@gmail.com>
2025-10-26 02:01:05 +0700

    Add unit tests for <Counter /> component

---
+++ src/components/Counter/Counter.test.tsx
@@ -0,0 +1,24 @@
+import { Counter } from '~/components/Counter/Counter'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('<Counter />', () => {
+  it('Increase, decrease couter value except not below 0', async () => {
+    const user = userEvent.setup()
+    render(<Counter />)
+
+    const increaseBtn = screen.getByRole('button', { name: '+' })
+    const decreaseBtn = screen.getByRole('button', { name: '-' })
+
+    // Increase counter value from 0 to 2
+    await user.click(increaseBtn)
+    await user.click(increaseBtn)
+
+    // Deacrease counter value 3 times, but it should not below 0
+    await user.click(decreaseBtn)
+    await user.click(decreaseBtn)
+    await user.click(decreaseBtn)
+
+    expect(screen.getByText(/count: 0/i)).toBeInTheDocument()
+  })
+})
